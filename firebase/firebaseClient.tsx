2243975bcb21c7c184430443f3ac689b821bb033
Jacob Skyrme <32583261+JacobSkyrme@users.noreply.github.com>
2024-02-27 16:02:48 +0000

    Update firebaseClient.tsx

--- firebase/firebaseClient.tsx
+++ firebase/firebaseClient.tsx
@@ -3,14 +3,14 @@ import "firebase/auth";
 
 
 const CLIENT_CONFIG = {
-  apiKey: "AIzaSyDkghzCTId_x9tqAcuJBBfkJCItzL5W1iE",
-  authDomain: "worktap-7e14f.firebaseapp.com",
-  databaseURL: "https://worktap-7e14f.firebaseio.com",
-  projectId: "worktap-7e14f",
-  storageBucket: "gs://worktap-7e14f.appspot.com/",
-  messagingSenderId: "974788484331",
-  appId: "1:974788484331:web:cec42e7a01cc012d94f2ae",
-  measurementId: "G-H34FYYPJFC"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
